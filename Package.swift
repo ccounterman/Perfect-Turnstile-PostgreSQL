75c8475a7fb4d30c9c5cd6a8128ecb95d3f2eda5
Dr. Craig A. Counterman <ccount@dimensionofmind.com>
2017-04-04 19:20:33 -0400

    depend on my Postgres-StORM

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "PerfectTurnstilePostgreSQL",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/SwiftORM/Postgres-StORM.git", majorVersion: 1),
+        .Package(url: "https://github.com/ccounterman/Postgres-StORM.git", majorVersion: 1),
         .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2),
         .Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 2),
         .Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 1, minor: 0),
